c694012dce30877ecae31929934ed84745c7f771
Diego Montebruni <montebrunidiego@gmail.com>
2015-11-10 17:47:57 -0200

    Criado Botão Cadastrar
    Senna, criei um botão cadastrar na tela inicial do programa, faltou ele,
    porque como a pessoa vai saber o login e senha sem ter um cadastro
    antes? Bjo Gato!

--- SistemaClube/src/conexao/ConnectionFactory.java
+++ SistemaClube/src/conexao/ConnectionFactory.java
@@ -21,6 +21,6 @@ public class ConnectionFactory {
     public static Connection getConnection() throws ClassNotFoundException, SQLException{
         Class.forName("com.mysql.jdbc.Driver");
         return DriverManager.getConnection("jdbc:mysql://localhost:3306/sistema_clube",
-                "root", "root");
+                "root", "aluno");
     }   
 }
